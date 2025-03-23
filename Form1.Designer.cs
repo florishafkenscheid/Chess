537fe91c73f078cc288f13dcbcedeaed9278216e
Jesper Meuzelaar <Jesper Meuzelaar@DESKTOP-FPRLFU6>
2025-03-23 23:55:19 +0100

    Made a new file called rounded-button, for a custom element

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            play_button = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(815, 175);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(290, 290);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // play_button
+            // 
+            play_button.BackColor = Color.FromArgb(132, 221, 99);
+            play_button.FlatAppearance.BorderSize = 0;
+            play_button.FlatStyle = FlatStyle.Flat;
+            play_button.Font = new Font("Arial", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            play_button.ForeColor = Color.White;
+            play_button.Location = new Point(568, 540);
+            play_button.Name = "play_button";
+            play_button.Size = new Size(784, 62);
+            play_button.TabIndex = 1;
+            play_button.Text = "Play";
+            play_button.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(238, 240, 242);
+            ClientSize = new Size(1894, 1009);
+            Controls.Add(play_button);
+            Controls.Add(pictureBox1);
+            Location = new Point(815, 175);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Button play_button;
     }
 }
