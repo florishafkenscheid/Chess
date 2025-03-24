42d2ccecdae60e1fe623bf9055c3dac2367cbef4
florishafkenscheid <floris@hafkenscheid.com>
2025-03-24 14:05:46 +0100

    chore: start on Board, Tile, Move, and pieces classes

---
+++ Models/Moves/Move.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessApp.Models.Board;
+
+namespace ChessApp.Models.Moves;
+
+internal class Move
+{
+    public Tile From { get; }
+    public Tile To { get; }
+    public char? Promotion { get; }
+
+    public Move(string moveString)
+    {
+        From = moveString.Substring(0, 2);
+        To = moveString.Substring(2, 2);
+        Promotion = moveString.Length > 4 ? moveString[4] : (char?)null;
+    }
+}
